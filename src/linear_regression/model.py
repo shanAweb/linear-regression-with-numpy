41e563f2b246c99c015df5fa5b67051c9d105b48
shanAweb <shanasif2002@gmail.com>
2026-05-16 14:53:17 +0500

    Update: Model completed

--- src/linear_regression/model.py
+++ src/linear_regression/model.py
@@ -1,11 +1,12 @@
 import numpy as np
 class LinearRegression:
-    def __init__(self, learning_rate = 0.01, n_iterations = 1000):
+    def __init__(self, learning_rate, n_iterations):
         self.learning_rate = learning_rate
         self.n_iterations = n_iterations
         self.weights = None
         self.bias = None
         self.loss_history = []
+
     def fit(self, X, y):
         n_sample, n_features = X.shape
         self.weights = np.zeros(n_features)
@@ -19,3 +20,40 @@ class LinearRegression:
             db = np.mean(y_pred - y)
             self.weights = self.weights - self.learning_rate * dw
             self.bias = self.bias - self.learning_rate * db
+
+    def predict(self, X):
+        if self.weights is None:
+            raise Exception("Model not trained yet. Call fit() first.")
+        return np.dot(X, self.weights) + self.bias
+    
+    def get_params(self):
+        if self.weights is None:
+            raise Exception("Model not trained yet. Call fit() first.")
+        return {
+            "weights": self.weights,
+            "bias": self.bias
+    }
+
+if __name__ == "__main__":
+    # Dummy data
+    X = np.array([[1], [2], [3], [4], [5]], dtype=np.float64)
+    y = np.array([2, 4, 6, 8, 10], dtype=np.float64)
+
+    # Train
+    model = LinearRegression(learning_rate=0.2, n_iterations=100000)
+    print("Iterations:", model.n_iterations)
+    print("Learning rate:", model.learning_rate)
+    model.fit(X, y)
+
+    # Predict
+    predictions = model.predict(X)
+    print("Predictions:", predictions)
+
+    # Params
+    params = model.get_params()
+    print("Weights:", params["weights"])
+    print("Bias:", params["bias"])
+
+    # Loss
+    print("Initial loss:", model.loss_history[0])
+    print("Final loss:", model.loss_history[-1])
